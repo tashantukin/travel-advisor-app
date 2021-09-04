25923f25826cf1d9e51799f7a26fb433fa644129
tashantukin <nmfnavarro@gmail.com>
2021-09-04 17:27:51 +0800

    add Map component
    map not working, need to fix account billing settings on google dev account

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'
+import LocationOnOutLinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab'
 
+
+import useStyles from './styles'
+
+
+
+ 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)')
+
+    const coordinates = {lat: 0, lng: 0}
+
+
     return (
-        <h1> Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                boostrapURLKeys={{key: 'AIzaSyAd7LmemOsoiwX4UMX84IFs8TBHW_k3SlY'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+
+
+
+            >
+
+            </GoogleMapReact>
+
+        </div>
+
+
     )
 }
 
